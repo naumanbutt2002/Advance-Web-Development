88b6e63fb4ae550c6e96d7498843bcb6b79f8f91
naumanbutt2002 <nauman100percent@gmail.com>
2024-04-23 16:30:35 -0400

    lesson-3_Example-7

--- Lesson-3_RunCustomCode/src/App.jsx
+++ Lesson-3_RunCustomCode/src/App.jsx
@@ -50,13 +50,30 @@
 // In this code, we used multiple classes separated by comma (,).
 // When you used “textAlign” property in inline CSS, then write it in camel casing style and without white space. But in case of external CSS, it will be written by using hyphen(-) i.e., text-align.
 
+// export default function App() {
+//   return (
+//     <div>
+//       <h1 style={{ margin: "25px 0px", textAlign: "center" }}>
+//         naumanbutt2002
+//       </h1>
+//       <p style={{ textAlign: "center" }}>Introduction about author</p>
+//     </div>
+//   );
+// }
+
+// Example Code 7:
+
+// Here, we did same work as in previous example, but by using external CSS file. Following are two files. One is App.css and other is App.js.
+
+// First line in both files are commented. Note the method of commenting, that is different in both files.
+
+import './App.css';
+
 export default function App() {
   return (
     <div>
-      <h1 style={{ margin: "25px 0px", textAlign: "center" }}>
-        naumanbutt2002
-      </h1>
-      <p style={{ textAlign: "center" }}>Introduction about author</p>
+      <h1>Muhammad Nauman</h1>
+      <p>Introduction about author</p>
     </div>
   );
 }
