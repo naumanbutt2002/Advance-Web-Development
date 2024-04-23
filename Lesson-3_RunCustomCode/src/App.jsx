fb2b131c0e8900081ba2446dac75ee6f63a623b8
naumanbutt2002 <nauman100percent@gmail.com>
2024-04-23 15:52:36 -0400

    lesson-3_Example-4

--- Lesson-3_RunCustomCode/src/App.jsx
+++ Lesson-3_RunCustomCode/src/App.jsx
@@ -8,9 +8,26 @@
 
 //Example Code 2: In the example, we displayed name of author for this website.
 
+// export default function App() {
+//   return (
+//     <h1>naumanbutt2002</h1>
+//   );
+// }
+
+//Example Code 4: Showing name of author at middle of the web-page.
+
+//Method 1: update App() function in App.js like as shown below. In this method, we used inline CSS.
+
+// function App() {
+//   return (
+//     <h1 style={{textAlign: 'center'}}>Muhammad Nauman</h1>
+//   );
+// }
+
+//Method 2: Use external CSS.
+
 export default function App() {
   return (
-    <h1>naumanbutt2002</h1>
+    <h1 className='App'>Muhammad Nauman</h1>
   );
 }
-
