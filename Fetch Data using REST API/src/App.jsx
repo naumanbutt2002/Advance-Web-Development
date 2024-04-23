4be784a61adbe42ae2b05c0fe81e8355dc1207d0
naumanbutt2002 <nauman100percent@gmail.com>
2024-04-23 14:47:25 -0400

    Fetch-Data_Example-3

--- Fetch Data using REST API/src/App.jsx
+++ Fetch Data using REST API/src/App.jsx
@@ -17,10 +17,31 @@
 
 //EXAMPLE-2
 
+// const getDataFromApi = async () => {
+//   let myEndpoint = "https://newsapi.org/v2/top-headlines?country=us&apiKey=dd4cf4a5a1e64635a4d62c631362bdf4";
+//   let myData = await fetch(myEndpoint);
+//   console.log("received Data: ", myData);
+// };
+ 
+// function App() {
+//   getDataFromApi();
+//   return (
+//     <div>
+//       <h1>Data from our REST API</h1>
+//     </div>
+//   );
+// }
+ 
+// export default App;
+
+//EXAMPLE-3
+
 const getDataFromApi = async () => {
   let myEndpoint = "https://newsapi.org/v2/top-headlines?country=us&apiKey=dd4cf4a5a1e64635a4d62c631362bdf4";
-  let myData = await fetch(myEndpoint);
-  console.log("received Data: ", myData);
+  let myData = await fetch(myEndpoint); /* Must wait for fetching */
+  //console.log("received Data: ", myData);
+  let parsedData = await myData.json(); /* Must wait for myData.json */
+  console.log("parsed received Data: ", parsedData);
 };
  
 function App() {
