cdcebbfaba1b476907e7e89062edb45c6f0a3076
naumanbutt2002 <nauman100percent@gmail.com>
2024-04-23 15:35:48 -0400

    lesson-3_Example-2

--- Lesson-3_RunCustomCode/src/App.jsx
+++ Lesson-3_RunCustomCode/src/App.jsx
@@ -1,7 +1,15 @@
 //Example Code 1: In the example, we display “Hello World”.
 
+// export default function App() {
+//   return (
+//     <h1>Hello World</h1>
+//   );
+// }
+
+//Example Code 2: In the example, we displayed name of author for this website.
+
 export default function App() {
   return (
-    <h1>Hello World</h1>
+    <h1>naumanbutt2002</h1>
   );
 }
