4a8c5451efbb200f4c0563cbdf524de8f733ea38
naumanbutt2002 <nauman100percent@gmail.com>
2024-04-23 14:50:57 -0400

    Fetch-Data_Example-4

--- Fetch Data using REST API/src/App.jsx
+++ Fetch Data using REST API/src/App.jsx
@@ -35,13 +35,44 @@
 // export default App;
 
 //EXAMPLE-3
+// const getDataFromApi = async () => {
+//   let myEndpoint = "https://newsapi.org/v2/top-headlines?country=us&apiKey=dd4cf4a5a1e64635a4d62c631362bdf4";
+//   let myData = await fetch(myEndpoint); /* Must wait for fetching */
+//   //console.log("received Data: ", myData);
+//   let parsedData = await myData.json(); /* Must wait for myData.json */
+//   console.log("parsed received Data: ", parsedData);
+// };
+ 
+// function App() {
+//   getDataFromApi();
+//   return (
+//     <div>
+//       <h1>Data from our REST API</h1>
+//     </div>
+//   );
+// }
+ 
+// export default App;
 
+//EXAMPLE-4
 const getDataFromApi = async () => {
   let myEndpoint = "https://newsapi.org/v2/top-headlines?country=us&apiKey=dd4cf4a5a1e64635a4d62c631362bdf4";
   let myData = await fetch(myEndpoint); /* Must wait for fetching */
   //console.log("received Data: ", myData);
   let parsedData = await myData.json(); /* Must wait for myData.json */
   console.log("parsed received Data: ", parsedData);
+ 
+  let dataStatus = parsedData.status;
+  console.log("value of 'status' in data: ", dataStatus);
+ 
+  let datatotalResults = parsedData.totalResults;
+  console.log("value of 'status' in data: ", datatotalResults);
+ 
+  let dataarticles = parsedData.articles;
+  console.log("value of 'articles' in data: ", dataarticles);
+   
+  let dataarticles_3 = parsedData.articles[3];
+  console.log("value of 'articles' at 3rd index: ", dataarticles_3);
 };
  
 function App() {
