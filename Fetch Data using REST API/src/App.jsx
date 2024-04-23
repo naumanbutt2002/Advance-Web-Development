7fd5f47fa960fcb5f45ac02b01457a85b7347da5
naumanbutt2002 <nauman100percent@gmail.com>
2024-04-23 14:39:26 -0400

    Fetch-Data_Example-1

--- Fetch Data using REST API/src/App.jsx
+++ Fetch Data using REST API/src/App.jsx
@@ -1,52 +1,12 @@
-import { useEffect, useState } from "react";
- 
-/* File: App.jsx */
 function App() {
-  const [dataStatus, setdataStatus] = useState("");
-  const [datatotalResults, setdatatotalResults] = useState("");
-  const [dataArticles, setArticles] = useState([]);
- 
-  const getDataFromApi = async () => {
-    let myEndpoint =
-      "https://newsapi.org/v2/top-headlines?country=us&apiKey=41ef462eca8142a4bbb7eaa25b4ece4c";
-    let myData = await fetch(myEndpoint); /* Must wait for fetching */
-    let parsedData = await myData.json(); /* Must wait for myData.json */
-    console.log("parsed received Data: ", parsedData);
- 
-    setdataStatus(parsedData.status);
-    setdatatotalResults(parsedData.totalResults);
-    setArticles(parsedData.articles);
-  };
- 
-  useEffect(() => {
-    getDataFromApi();
-  }, []);
- 
+  let myEndpoint = "https://newsapi.org/v2/top-headlines?country=us&apiKey=41ef462eca8142a4bbb7eaa25b4ece4c";
+  let myData = fetch(myEndpoint);
+  console.log("received Data: ", myData);
+  //let parsedData = myData.json();
+  //console.log("received parseData: ", parsedData);
   return (
     <div>
       <h1>Data from our REST API</h1>
- 
-      <p>
-        <b>Status:</b> {dataStatus}
-      </p>
-      <p>
-        <b>Total Results:</b> {datatotalResults}
-      </p>
-      <p>
-        <b>Articles:</b>
-      </p>
-      {
-        dataArticles.map((element, uniqueKey = 0) => {
-          uniqueKey = uniqueKey + 1;
-          return (
-            <div key={uniqueKey}>
-              <p>
-                {uniqueKey}. {element.title}
-              </p>
-            </div>
-          );
-        })
-      }
     </div>
   );
 }
