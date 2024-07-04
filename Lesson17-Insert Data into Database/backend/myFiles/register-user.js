23f1ca1514cf78ded64e9801325766bab5d38fe0
naumanbutt2002 <nauman100percent@gmail.com>
2024-07-04 11:54:21 -0400

    Lesson-17: Ex2

--- Lesson17-Insert Data into Database/backend/myFiles/register-user.js
+++ Lesson17-Insert Data into Database/backend/myFiles/register-user.js
@@ -11,15 +11,16 @@ myRouter.post('/', [
     body("name", "Please write your name").exists()
 ],
     async (req, res) => {
+        let mySuccess = false
         const myError = validationResult(req) //getting error by validationResult over "req"
         if (!myError.isEmpty()) {//if error exists
             //error status along with errors in array format
             //sending bad request (code: 400) with some JSON
-            return res.status(400).json({ errorExistAsBelow: myError.array() })
+            return res.status(400).json({ errorExistAsBelow: myError.array(), success: mySuccess })
         }
         try {
             let myemail = req.body.email
-            console.log("myemail: ", myemail)
+            //console.log("myemail: ", myemail)
             let myname = req.body.name
             let myfname = req.body.fname
             let mypassword = req.body.password
@@ -32,11 +33,27 @@ myRouter.post('/', [
                 password: mypassword,
                 status: mystatus
             }
-            let myData = myUser(savingData)
-            myData.save()
-        } catch (e) {
-            console.log("Error")
+            //let myData = myUser(savingData)
+            //myData.save()
+ 
+            //using Promises
+            let myNewUser = await myUser.create(savingData)
+ 
+            mySuccess = true
+ 
+            //create data for response
+            let resData = {
+                message: "Data has been inserted successfully",
+                insertedData: savingData,
+                success: mySuccess,
+                newUserFromData: myNewUser
+            }
+            res.json(resData)
+        } catch (myError) {
+            console.log(myError.message)
+            res.status(500).send({ success: mySuccess, message: "some error occured" })
         }
+ 
     })
  
 //exporting so that it can access from other files
